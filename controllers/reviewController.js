7b92ea435d6fd8c49021dbabfddf0d43ff5d0c3a
taurusilver7 <prasad20697@gmail.com>
2021-04-10 16:10:15 +0530

    review handle fns created

---
+++ controllers/reviewController.js
@@ -0,0 +1,45 @@
+const Review = require('../models/reviewModel');
+const factory = require('./handleFactory');
+
+exports.getReviews = factory.getAll(Review);
+// exports.getReviews = catchAsync(async (req, res, next) => {
+//   let filter = {};
+//   if (req.params.tourId) filter = { tour: req.params.tourId };
+
+//   const reviews = await Review.find(filter);
+
+//   res.status(200).json({
+//     status: 'success',
+//     results: reviews.length,
+//     data: {
+//       reviews,
+//     },
+//   });
+// });
+
+exports.setTourUserIds = (req, res, next) => {
+  // Allowed nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.params.id;
+  next();
+};
+
+exports.getReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+// exports.createReview = catchAsync(async (req, res, next) => {
+//   // Allow nested route
+//   if (!req.body.tour) req.body.tour = req.params.tourId;
+//   if (!req.body.user) req.body.user = req.params.id;
+
+//   const newReview = await Review.create(req.body);
+
+//   res.status(201).json({
+//     status: 'success',
+//     data: {
+//       review: newReview,
+//     },
+//   });
+// });
+
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
