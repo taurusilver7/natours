fb509bd98a3a16ee75456cc26f12cfd263db6775
taurusilver7 <prasad20697@gmail.com>
2021-04-10 16:30:47 +0530

    root fn file added

---
+++ public/js/index.js
@@ -0,0 +1,72 @@
+/*eslint - disable;*/
+import '@babel/polyfill';
+import { displayMap } from './mapBox';
+import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
+
+// DOM elements
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form--login');
+const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const passwordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
+
+// Delegation
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+// a event to listen to submit event from login form
+if (loginForm)
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // values
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    login(email, password);
+  });
+
+if (logoutBtn) logoutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    // add photo to upload data
+    form.append('photo', document.getElementById('photo').files[0]);
+    // console.log(form);
+    updateSettings(form, 'data');
+  });
+
+if (passwordForm)
+  passwordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating..';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+
+if (bookBtn)
+  bookBtn.addEventListener('click', (e) => {
+    e.target.textContent = 'Processing..';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
