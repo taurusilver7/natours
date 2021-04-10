3316aeaf12d1d617510e3a9cbf3157b5b47c9665
taurusilver7 <prasad20697@gmail.com>
2021-04-10 16:32:14 +0530

    email feature enable

---
+++ utils/email.js
@@ -0,0 +1,98 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+// new Email(user, url).sendWelcome();
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Taurusilver <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // use sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+    return nodemailer.createTransport({
+      // service: 'Gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // render the html based on pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // create a transport & send email.
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours family');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token, valid for 10 mins.'
+    );
+  }
+};
+/*
+const sendEmail = async (options) => {
+  // 1.Create a transporter
+  const transporter = pug.createTransport({
+    // service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    // activate in gmail "less secure app" option
+  });
+
+  // Define an email option
+  const mailOptions = {
+    from: 'Taurusilver <taurus@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    //   html:
+  };
+
+  // 3.Send the email.
+  await transporter.sendMail(mailOptions);
+};
+*/
