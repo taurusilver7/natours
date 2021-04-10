dfe573837ca82411689dd7f629eb74af066fbbbb
taurusilver7 <prasad20697@gmail.com>
2021-04-10 16:30:02 +0530

    payment fn created

---
+++ public/js/stripe.js
@@ -0,0 +1,27 @@
+/*eslint-disable */
+/* the payment process on the front end, issued from the tour.pug
+The downloaded script exposes a stripe object to the global scope 
+*/
+
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+// stripe publish key for front-wend
+export const bookTour = async (tourId) => {
+  try {
+    const stripe = Stripe(process.env.STRIPE_PUBLIC_KEY);
+    // 1. get checkout session from end-point/api
+    const session = await axios(
+      `/api/v1/bookings/checkout-session/${tourId}`
+    );
+    // console.log(session);
+
+    // 2. create a checkout-form + process(charge crdt card) using stripe object
+    return await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
