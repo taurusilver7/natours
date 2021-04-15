8e979a92b6408e46bd0f26aece7735fc6262c20f
taurusilver7 <prasad20697@gmail.com>
2021-04-15 11:32:31 +0530

    stripe updated

--- utils/email.js
+++ utils/email.js
@@ -9,7 +9,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Taurusilver <${process.env.EMAIL_FROM}>`;
+    this.from = `Tauru Silver <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
