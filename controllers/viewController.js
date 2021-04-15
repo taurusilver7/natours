d1a80b567c6c89051a5a8169870c5edd54fef1a2
taurusilver7 <prasad20697@gmail.com>
2021-04-15 17:19:38 +0530

    controller updated

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -29,7 +29,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
   // 1. get the data, for the requested tour (incl reviews & guides.)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user',
+    fields: 'rating review user',
   });
   // error handling
   if (!tour) next(new AppError('There is no tour with that name.', 404));
@@ -38,10 +38,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
   // 3. Render the template using the data.
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
+    .set('Content-Security-Policy')
     .render('tour', {
       title: `${tour.name} Tour`,
       tour,
