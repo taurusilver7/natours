0ccb929049f25461f0ec6acbca597ef63dfe881c
taurusilver7 <prasad20697@gmail.com>
2021-04-15 11:33:13 +0530

    web page route updated

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
@@ -13,12 +12,10 @@ const router = express.Router();
 //   });
 // });
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewController.getOverview
-);
+router.use(viewController.alerts);
+
+router.get('/', authController.isLoggedIn, viewController.getOverview);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
