96fdcfe7a91ede6ac128da11dd07a4182249241b
taurusilver7 <prasad20697@gmail.com>
2021-04-15 11:35:34 +0530

    alert feature updated

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,5 +1,5 @@
+/* eslint - disable */
 // create an alert to display successful logging in
-import '@babel/polyfill';
 
 export const hideAlert = () => {
   const ele = document.querySelector('.alert');
@@ -7,9 +7,9 @@ export const hideAlert = () => {
 };
 
 // type = 'success' or 'error'
-export const showAlert = (type, msg) => {
+export const showAlert = (type, msg, time = 7) => {
   hideAlert();
   const markup = `<div class='alert alert--${type}'>${msg}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-  window.setTimeout(hideAlert, 5000);
+  window.setTimeout(hideAlert, time * 1000);
 };
